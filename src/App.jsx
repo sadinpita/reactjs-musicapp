11648d7263000b7a13d25ae7ebb9522d428e9231
sadin97 <sadinpita_97@windowslive.com>
2019-01-08 14:35:45 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap'
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+export default App;
