d0f947f34339489a66d72b0e71d4c4d351d0e048
sadin97 <sadinpita_97@windowslive.com>
2019-01-08 15:02:51 +0100

    adding elements, implementing react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Form, FormControl, Button } from 'react-bootstrap'
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   constructor (props) {
@@ -12,6 +12,28 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <div className="App-title">Music App</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+            />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+
+        <div className="Profile">
+          <div>
+            <div>Artist Picture</div>
+            <div>Artist Name</div>
+          </div>
+          <div className="Gallery">
+            Gallery
+          </div>
+        </div>
       </div>
     )
   }
