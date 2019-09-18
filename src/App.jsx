223b74e5f2ba7e8139e772e12fc53db39e797252
sadin97 <sadinpita_97@windowslive.com>
2019-09-18 16:58:26 +0200

    Adding fetch get method, testing API

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import MusicCard from './components/MusicCard';
 import 'typeface-roboto';
 
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles(theme => ({
-     container: {
-          display: 'flex',
-          flexWrap: 'wrap',
-     },
-     textField: {
-          marginLeft: theme.spacing(1),
-          marginRight: theme.spacing(1),
-          width: 200,
-     },
-     dense: {
-          marginTop: 19,
-     },
-     menu: {
-          width: 200,
-     },
-}));
+import Button from '@material-ui/core/Button';
 
 
+class App extends Component {
+     constructor (props) {
+          super (props);
+          this.state = {
+               query: ''
+          }
+     }     
 
-function App() {
-     const classes = useStyles();
-     const [values, setValues] = React.useState({
-          name: '',
-          age: '',
-          multiline: 'Controlled',
-     });
+     search () {
+          console.log('this.state: ', this.state);
+          const BASE_URL = 'https://deezerdevs-deezer.p.rapidapi.com/search?';
+          const FETCH_URL = BASE_URL + 'q=' + this.state.query;
+          console.log('FETCH_URL: ', FETCH_URL);
+          fetch(FETCH_URL, {
+               method: 'GET',
+               headers: {
+                    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+                    'X-RapidAPI-Key': '54aa7e8bc6mshd812b154b8704c0p12b1b8jsn505667115014'
+               }
+          }).then (response => console.log('response: ', response));
+     }
 
-     const handleChange = name => event => {
-          setValues({ ...values, [name]: event.target.value });
-     };
-
-     return (
-          <div className="App">
-               <header className="App-header">
+     render () {
+          return (
+               <div id="app-wrapper">
                     <div>
-                         <form className={classes.container} noValidate autoComplete="off">
+                         <form class="search-container" noValidate autoComplete="off">
                               <TextField
                                    id="standard-name"
                                    label="Search for song"
-                                   className={classes.textField}
-                                   value={values.name}
-                                   onChange={handleChange('name')}
                                    margin="normal"
+                                   value={this.state.query}
+                                   onChange={event => { this.setState({query: event.target.value}) }}
                               />
+                              <Button
+                                   variant="contained"
+                                   onClick={() => {this.search()}}
+                                   className="search-btn">
+                                   Search
+                              </Button>
                          </form>
+                         
                     </div>
                     <MusicCard></MusicCard>
-               </header>
-          </div>
-     );
+               </div>
+          )
+     }
 }
 
 export default App;
